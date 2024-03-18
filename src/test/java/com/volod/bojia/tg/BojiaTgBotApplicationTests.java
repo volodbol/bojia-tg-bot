8c80a9c4c58c7321eaa5f78f31893dfc2efcf520
Volodymyr Bolshakov <volodbol@gmail.com>
2024-03-18 20:38:35 +0200

    fix: remove spring boot test

--- src/test/java/com/volod/bojia/tg/BojiaTgBotApplicationTests.java
+++ src/test/java/com/volod/bojia/tg/BojiaTgBotApplicationTests.java
@@ -1,11 +1,9 @@
 package com.volod.bojia.tg;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 class BojiaTgBotApplicationTests {
 
 	@Test
