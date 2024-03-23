0ae89f582cfe759bb5d824023bc515c1ec247ae1
Volodymyr Bolshakov <volodbol@gmail.com>
2024-03-23 16:07:18 +0200

    fix: remove sleep on shutdown

--- src/main/java/com/volod/bojia/tg/service/shutdown/BojiaShutdownService.java
+++ src/main/java/com/volod/bojia/tg/service/shutdown/BojiaShutdownService.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -16,10 +14,9 @@ public class BojiaShutdownService {
     private final TelegramBot bojiaTelegramBot;
 
     @PreDestroy
-    public void onShutdown() throws InterruptedException {
+    public void onShutdown() {
         this.bojiaTelegramBot.shutdown();
         LOGGER.info("BojIA telegram bot - shutdown initiated");
-        TimeUnit.SECONDS.sleep(5);
     }
 
 }
