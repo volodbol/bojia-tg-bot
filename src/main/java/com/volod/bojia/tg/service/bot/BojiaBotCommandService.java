668dbcc25b2dce04739b76119a3e973c876892cf
Volodymyr Bolshakov <volodbol@gmail.com>
2024-04-20 18:16:16 +0300

    feat: create methods to process help command

--- src/main/java/com/volod/bojia/tg/service/bot/BojiaBotCommandService.java
+++ src/main/java/com/volod/bojia/tg/service/bot/BojiaBotCommandService.java
@@ -1,7 +1,54 @@
 package com.volod.bojia.tg.service.bot;
 
+import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
+import com.volod.bojia.tg.service.exception.BojiaExceptionHandlerService;
 
-public interface BojiaBotCommandService {
-    Integer processUpdate(Update update);
+import java.util.Map;
+import java.util.function.Consumer;
+
+import static com.volod.bojia.tg.domain.bot.BojiaBotMyCommand.HELP;
+import static java.util.Objects.isNull;
+
+public abstract class BojiaBotCommandService {
+
+    protected final TelegramBot bot;
+    protected final BojiaExceptionHandlerService exceptionHandlerService;
+
+    private final Map<String, Consumer<Update>> commandMappings = Map.of(
+            HELP.getCommand(), this::processHelpCommand
+    );
+
+    protected BojiaBotCommandService(
+            TelegramBot bot,
+            BojiaExceptionHandlerService exceptionHandlerService
+    ) {
+        this.bot = bot;
+        this.exceptionHandlerService = exceptionHandlerService;
+    }
+
+    public final Integer processUpdate(Update update) {
+        try {
+            this.validateUpdate(update);
+            var message = update.message();
+            var command = message.text().split(" ")[0];
+            this.commandMappings.getOrDefault(command, this::processUnknownCommand).accept(update);
+            return update.updateId();
+        } catch (RuntimeException ex) {
+            this.exceptionHandlerService.publishException(ex);
+            this.processUnknownCommand(update);
+            return update.updateId();
+        }
+    }
+
+    public final void validateUpdate(Update update) {
+        var message = update.message();
+        if (isNull(message) || isNull(message.text())) {
+            throw new IllegalArgumentException("Illegal update: " + update);
+        }
+    }
+
+    public abstract void processHelpCommand(Update update);
+    public abstract void processUnknownCommand(Update update);
+
 }
--- src/main/java/com/volod/bojia/tg/service/bot/impl/BojiaBotCommandServiceImpl.java
+++ src/main/java/com/volod/bojia/tg/service/bot/impl/BojiaBotCommandServiceImpl.java
@@ -2,26 +2,45 @@ package com.volod.bojia.tg.service.bot.impl;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.volod.bojia.tg.service.bot.BojiaBotCommandService;
-import lombok.RequiredArgsConstructor;
+import com.volod.bojia.tg.service.exception.BojiaExceptionHandlerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
-public class BojiaBotCommandServiceImpl implements BojiaBotCommandService {
+public class BojiaBotCommandServiceImpl extends BojiaBotCommandService {
 
-    private final TelegramBot bot;
+    @Autowired
+    public BojiaBotCommandServiceImpl(
+            TelegramBot bot,
+            BojiaExceptionHandlerService exceptionHandlerService
+    ) {
+        super(
+                bot,
+                exceptionHandlerService
+        );
+    }
 
     @Override
-    public Integer processUpdate(Update update) {
+    public void processHelpCommand(Update update) {
         this.bot.execute(
                 new SendMessage(
                         update.message().chat().id(),
-                        "Message received"
+                        "Help command received"
                 )
         );
-        return update.updateId();
+    }
+
+    @Override
+    public void processUnknownCommand(Update update) {
+        this.bot.execute(
+                new SendMessage(
+                        update.message().chat().id(),
+                        "Unknown command received\\. Send `/help` to see available commands!`"
+                ).parseMode(ParseMode.MarkdownV2)
+        );
     }
 
 }
