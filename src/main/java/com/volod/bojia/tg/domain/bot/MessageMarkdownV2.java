45fa81410f77d0be07a1ea6854ac80a47326d9e1
Volodymyr Bolshakov <volodbol@gmail.com>
2024-04-28 19:28:05 +0300

    feat: create markdown v2 message

---
+++ src/main/java/com/volod/bojia/tg/domain/bot/MessageMarkdownV2.java
@@ -0,0 +1,77 @@
+package com.volod.bojia.tg.domain.bot;
+
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.request.SendMessage;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.regex.Pattern;
+
+import static java.util.Objects.nonNull;
+
+@Getter
+@EqualsAndHashCode
+@ToString
+public class MessageMarkdownV2 {
+    private final Object chatId;
+    private final String value;
+
+    private MessageMarkdownV2(Object chatId, String value) {
+        this.chatId = chatId;
+        this.value = value;
+    }
+
+    public static MessageMarkdownV2Builder builder() {
+        return new MessageMarkdownV2Builder();
+    }
+
+    public SendMessage toSendMessage() {
+        return new SendMessage(this.chatId, this.value).parseMode(ParseMode.MarkdownV2);
+    }
+
+    public static class MessageMarkdownV2Builder {
+        private static final Pattern ESCAPE_PATTERN = Pattern.compile("([_*\\[\\]()~`>#+\\-=|{}.!])");
+
+        private Object chatId;
+        private final StringBuilder builder;
+
+        private MessageMarkdownV2Builder() {
+            this.builder = new StringBuilder();
+        }
+
+        private String escape(String value) {
+            return ESCAPE_PATTERN.matcher(value).replaceAll("\\\\$1");
+        }
+
+        public MessageMarkdownV2Builder chatId(Object chatId) {
+            this.chatId = chatId;
+            return this;
+        }
+
+        public MessageMarkdownV2Builder text(String value) {
+            this.builder.append(this.escape(value));
+            return this;
+        }
+
+        public MessageMarkdownV2Builder inlineCode(String value) {
+            this.builder.append("`%s`".formatted(this.escape(value)));
+            return this;
+        }
+
+        public MessageMarkdownV2Builder bold(String value) {
+            this.builder.append("*%s*".formatted(this.escape(value)));
+            return this;
+        }
+
+        public MessageMarkdownV2 build() {
+            if (nonNull(this.chatId)) {
+                var message = this.builder.toString();
+                return new MessageMarkdownV2(this.chatId, message);
+            } else {
+                throw new IllegalArgumentException("ChatId is null");
+            }
+        }
+
+    }
+}
---
+++ src/test/java/com/volod/bojia/tg/domain/bot/MessageMarkdownV2Test.java
@@ -0,0 +1,79 @@
+package com.volod.bojia.tg.domain.bot;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MessageMarkdownV2Test {
+
+    private static Stream<Arguments> addTextTest() {
+        return Stream.of(
+                Arguments.of(
+                        "To generate cover letter we have to know something about you.",
+                        "To generate cover letter we have to know something about you\\."
+                )
+        );
+    }
+
+    private static Stream<Arguments> addInlineCodeTest() {
+        return Stream.of(
+                Arguments.of(
+                        "To generate cover letter we have to know something about you.",
+                        "`To generate cover letter we have to know something about you\\.`"
+                )
+        );
+    }
+
+    private static Stream<Arguments> addBoldTest() {
+        return Stream.of(
+                Arguments.of(
+                        "To generate cover letter we have to know something about you.",
+                        "*To generate cover letter we have to know something about you\\.*"
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("addTextTest")
+    void addTextTest(String text, String expected) {
+        // Arrange
+        var builder = MessageMarkdownV2.builder().chatId(1).text(text);
+
+        // Act
+        var actual = builder.build();
+
+        // Assert
+        assertThat(actual.getValue()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("addInlineCodeTest")
+    void addInlineCodeTest(String text, String expected) {
+        // Arrange
+        var builder = MessageMarkdownV2.builder().chatId(1).inlineCode(text);
+
+        // Act
+        var actual = builder.build();
+
+        // Assert
+        assertThat(actual.getValue()).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("addBoldTest")
+    void addBoldTest(String text, String expected) {
+        // Arrange
+        var builder = MessageMarkdownV2.builder().chatId(1).bold(text);
+
+        // Act
+        var actual = builder.build();
+
+        // Assert
+        assertThat(actual.getValue()).isEqualTo(expected);
+    }
+
+}
