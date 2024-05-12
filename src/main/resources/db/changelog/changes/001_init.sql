2b86ef3b4f5d3bc6f69c11e7c21c58a91da5776a
Volodymyr Bolshakov <volodbol@gmail.com>
2024-05-12 14:32:51 +0300

    fix: add fk constraint, make keywords size 1024

--- src/main/resources/db/changelog/changes/001_init.sql
+++ src/main/resources/db/changelog/changes/001_init.sql
@@ -6,16 +6,17 @@ CREATE TABLE IF NOT EXISTS bojia_bot_details
 
 CREATE TABLE IF NOT EXISTS bojia_bot_user
 (
-    id         INTEGER NOT NULL PRIMARY KEY,
-    chat_id    INTEGER NOT NULL,
-    first_name VARCHAR(512) NOT NULL,
+    id         INTEGER       NOT NULL PRIMARY KEY,
+    chat_id    INTEGER       NOT NULL,
+    first_name VARCHAR(512)  NOT NULL,
     prompt     VARCHAR(2048) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS bojia_bot_user_search
 (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    provider VARCHAR(512) NOT NULL,
-    keywords VARCHAR(512) NOT NULL
+    id       SERIAL PRIMARY KEY,
+    user_id  INTEGER       NOT NULL,
+    provider VARCHAR(512)  NOT NULL,
+    keywords VARCHAR(1024) NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES bojia_bot_user (id)
 );
