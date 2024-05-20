aa68be148dd7f1429d3a405c8883f1fa8cb142d0
Volodymyr Bolshakov <volodbol@gmail.com>
2024-05-20 22:11:42 +0300

    feat: include openai auto configuration

--- run.sh
+++ run.sh
@@ -6,13 +6,15 @@ DATABASE_URL="jdbc:postgresql://localhost:5432/$DATABASE_NAME"
 DATABASE_USERNAME="postgres"
 DATABASE_PASSWORD="postgres"
 TELEGRAM_BOT_TOKEN="token-text"
+OPENAI_API_KEY="api-key"
 
 set -- \
   --spring.profiles.active="$PROFILE" \
   --spring.datasource.url="$DATABASE_URL" \
   --spring.datasource.username="$DATABASE_USERNAME" \
   --spring.datasource.password="$DATABASE_PASSWORD" \
-  --bojia.bot-token="$TELEGRAM_BOT_TOKEN"
+  --bojia.bot-token="$TELEGRAM_BOT_TOKEN" \
+  --spring.ai.openai.api-key="$OPENAI_API_KEY"
 
 echo "./mvnw spring-boot:run -Dspring-boot.run.arguments=\"$*\""
 
--- src/main/java/com/volod/bojia/tg/BojiaTgBotApplication.java
+++ src/main/java/com/volod/bojia/tg/BojiaTgBotApplication.java
@@ -1,14 +1,11 @@
 package com.volod.bojia.tg;
 
 import com.volod.bojia.tg.property.BojiaApplicationProperties;
-import org.springframework.ai.autoconfigure.openai.OpenAiAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(
-        exclude = OpenAiAutoConfiguration.class
-)
+@SpringBootApplication
 @EnableConfigurationProperties({
         BojiaApplicationProperties.class
 })
